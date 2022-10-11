770c6b93d93ea66924dff85a5d27005921d346e3
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-11 10:46:22 +0900

    Update 백준_로봇청소기_221011.py

--- BFS-DFS/백준_로봇청소기_221011.py
+++ BFS-DFS/백준_로봇청소기_221011.py
@@ -4,8 +4,32 @@ x, y, dic = map(int, input().split())
 
 map = [list(map(int, input().split())) for _ in range(n)]
 
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
+visited = [[False for _ in range(m)]for _ in range(n)]
+visited[x][y] = True
 
+answer = 1
 
+while True:
+    is_clean = False
+    for i in range(4):
+        nx = x + dx[(dic + 3) % 4]
+        ny = y + dy[(dic + 3) % 4]
+        dic = (dic + 3) % 4
+        if 0<= nx < n and 0<= ny < m:
+            if not visited[nx][ny] and map[nx][ny] != 1:
+                visited[nx][ny] = True
+                answer += 1
+                is_clean = True
+                x = nx
+                y = ny
+                break
 
-print(x)
+    if not is_clean:  # 4방향 모두 청소가 되어 있을 때,
+        if map[x - dx[dic]][y - dy[dic]] == 1:  # 후진했는데 벽
+            print(answer)
+            break
+        else:
+            x, y = x - dx[dic], y - dy[dic]
